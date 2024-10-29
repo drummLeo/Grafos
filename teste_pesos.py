99fa4b98564943b3b421e1fe246779fbad4db165
Leonardo Drummond <94931650+drummLeo@users.noreply.github.com>
2024-10-28 23:12:46 -0300

    Create teste_pesos.py

---
+++ teste_pesos.py
@@ -0,0 +1,18 @@
+from grafos_com_pesos import GrafoComPesos
+
+k = 10
+for i in range(1, 2):
+    g = GrafoComPesos(f"textos/grafo_W_{i}.txt")
+    g.lista_adjacencia()
+
+    for j in range(20, 61, 10):
+        dijkstra = g.dijkstra_heap(10)
+        print(f"Distância/Caminho mínimo (Grafo {i} - Vértice {j}): {dijkstra[0][j-1]}/{g.obter_caminho(dijkstra[1], j)}")
+
+    print(f"Tempo médio (Grafo {i}):", g.tempo_medio_dijkstra(k))
+    del g
+
+g = GrafoComPesos("textos/rede_colaboracao.txt")
+g.lista_adjacencia()
+for autor in ("Alan M. Turing", "J. B. Kruskal", "Jon M. Kleinberg", "Éva Tardos", "Daniel R. Figueiredo"):
+    print(g.calcular_distancias_entre_pesquisadores("Edsger W. Dijkstra", autor))
